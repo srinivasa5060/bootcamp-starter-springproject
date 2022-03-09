affe4d868e32315d3ed53d65ce1ebae86e6cc7be
SMurthi1 <srinivasa.murthi1@t-mobile.com>
2022-03-08 21:27:12 -0700

    Initial checkin

---
+++ src/main/java/com/tmo/bootcamp/simplespringbootservice/SimpleSpringBootServiceApplication.java
@@ -0,0 +1,12 @@
+package com.tmo.bootcamp.simplespringbootservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleSpringBootServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tmo/bootcamp/simplespringbootservice/controller/BooksController.java
@@ -0,0 +1,35 @@
+package com.tmo.bootcamp.simplespringbootservice.controller;
+import com.tmo.bootcamp.simplespringbootservice.model.Book;
+import com.tmo.bootcamp.simplespringbootservice.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class BooksController {
+
+    @Autowired
+     private BookRepository repository;
+
+    @PostMapping("/api/books")
+    public ResponseEntity<Book> addBooks(@RequestBody Book book) {
+        book =repository.save(book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/api/books")
+    public List<Book> getAllBooks() {
+        return repository.findAll();
+    }
+
+
+    @DeleteMapping("/api/books")
+    public ResponseEntity<String> removeBooks() {
+        repository.deleteAll();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+}
